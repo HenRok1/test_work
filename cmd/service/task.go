a51b14601c12f9c6991eff58ce1ff508ebd01830
roberter <roberter@student.21-school.ru>
2023-05-02 19:05:38 +0300

    finish 1 part

--- cmd/service/task.go
+++ cmd/service/task.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"github.com/fsnotify/fsnotify"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
 	"gopkg.in/yaml.v3"
 	"log"
 	"os"
 	"os/exec"
-	"strings"
+	"time"
 )
 
 type Config struct {
@@ -16,23 +17,47 @@ type Config struct {
 	Commands []string `yaml:"commands"`
 }
 
+type FileChange struct {
+	ID          int       `db:"id"`
+	FilePath    string    `db:"file_path"`
+	Method      string    `db:"method"`
+	Time_change time.Time `db:"time_change"`
+}
+
 func main() {
+	//Чтение yaml-файла
 	f, err := os.ReadFile("config.yaml")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Here")
+	//Подключение к БД
+	db, err := sqlx.Connect("postgres", "user=postgres dbname=test sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	fmt.Println("Here2")
+
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS file_changes_2 (
+	id SERIAL PRIMARY KEY,
+	file_path TEXT NOT NULL,
+	method TEXT NOT NULL,
+	time_change TIMESTAMP WITH TIME ZONE NOT NULL
+ )`)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	var conf Config
-
 	if err := yaml.Unmarshal(f, &conf); err != nil {
 		log.Fatal(err)
 	}
 
 	//fmt.Printf("%+v\n", conf)
 
-	// Create new watcher.
+	// Создание нового Watcher
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
 		log.Fatal(err)
@@ -45,10 +70,11 @@ func main() {
 			select {
 			case event, ok := <-watcher.Events:
 				if !ok {
+					fmt.Println("finishhh")
 					return
 				}
 				log.Println("event:", event)
-				if event.Has(fsnotify.Write) {
+				if event.Has(fsnotify.Write) || event.Has(fsnotify.Chmod) || event.Has(fsnotify.Create) || event.Has(fsnotify.Remove) || event.Has(fsnotify.Rename) {
 					log.Println("modified file:", event.Name)
 
 					//Запуск команды
@@ -63,18 +89,16 @@ func main() {
 
 						fmt.Printf("%s\n", stdoutStderr)
 					}
-				}
-
-				fmt.Print("Enter 'q' to quit: ")
-				text, err := reader.ReadString('\n')
-				if err != nil {
-					fmt.Println(err)
-					continue
-				}
-				text = strings.TrimSpace(text)
-				if text == "q" {
-					fmt.Println("Exiting program...")
-					os.Exit(0)
+					change := &FileChange{FilePath: conf.Path, Method: event.String(), Time_change: time.Now().UTC()}
+					_, err = db.NamedExec(`INSERT INTO file_changes_2(file_path, method, time_change)
+												VALUES (:file_path, :method, :time_change)`, map[string]interface{}{
+						"file_path":   change.FilePath,
+						"method":      change.Method,
+						"time_change": change.Time_change.Format(time.RFC3339),
+					})
+					if err != nil {
+						log.Println(err)
+					}
 				}
 
 			case err, ok := <-watcher.Errors:
